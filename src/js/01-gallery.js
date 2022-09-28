c32346eeff2a6314fc646692c51ec3bba921b94e
Yuliya-Khovrych <105730156+Yuliya-Khovrych@users.noreply.github.com>
2022-09-29 00:09:44 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,10 +7,10 @@ import { galleryItems } from './gallery-items';
 //console.log(galleryItems);
 
 
-// import { gallery } from './customFunction/gallery-hw7';
-// import { markupGallery } from './customFunction/gallery-hw7';
-// import { galleryClick } from './customFunction/gallery-hw7';
-// console.log(galleryClick);
+import { gallery } from './customFunction/gallery-hw7';
+import { markupGallery } from './customFunction/gallery-hw7';
+import { galleryClick } from './customFunction/gallery-hw7';
+console.log(galleryClick);
 
 //namespace import - импорт всего
 // import * as all from './customFunction/gallery-hw7';
