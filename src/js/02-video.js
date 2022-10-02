aab39531c010ac64a6b85c1c3b1923a2193563de
Yuliya-Khovrych <ulia.voitishenko@gmail.com>
2022-10-03 01:24:57 +0300

    homework 8 js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+//console.log(Player);
+var throttle = require('lodash.throttle');
+//console.log(throttle);
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+iframe.addEventListener('click', player.on);
+
+player.on('timeupdate', function (data) {
+  throttle(
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(data.seconds)
+    ),
+    1000
+  );
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  console.log(savedTime);
+  console.log('played the video!');
+});
+
+if (localStorage.getItem('videoplayer-current-time') !== null) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,50 @@
+var throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const message = document.querySelector('textarea');
+form.addEventListener('input', onInput);
+form.addEventListener('submit', onSubmit);
+
+function onInput(evt) {
+  evt.preventDefault();
+  const { email, message } = evt.currentTarget.elements;
+  const data = {
+    email: email.value,
+    message: message.value,
+  };
+  const saveData = throttle(
+    localStorage.setItem('feedback-form-state', JSON.stringify(data)),
+    500
+  );
+}
+
+if (localStorage.getItem('feedback-form-state') !== null) {
+  try {
+    const parsedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+    //console.log(parsedData);
+    input.value = parsedData.email;
+    message.value = parsedData.message;
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+  }
+} else {
+  input.value = '';
+  message.value = '';
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  const { email, message } = evt.currentTarget.elements;
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+  const data = {
+    email: email.value,
+    message: message.value,
+  };
+  console.log(data);
+  evt.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
