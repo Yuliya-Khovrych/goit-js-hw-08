2358e4f9177d3adb8af4a1aede6587d7bd450904
Yuliya-Khovrych <ulia.voitishenko@gmail.com>
2022-10-04 15:49:33 +0300

    corrected 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,38 +1,39 @@
 var throttle = require('lodash.throttle');
+console.log(throttle);
 
 const form = document.querySelector('.feedback-form');
 const input = document.querySelector('input');
 const message = document.querySelector('textarea');
-form.addEventListener('input', onInput);
+form.addEventListener('input', throttle(onInput), 500);
 form.addEventListener('submit', onSubmit);
 
 function onInput(evt) {
   evt.preventDefault();
-  const { email, message } = evt.currentTarget.elements;
-  const data = {
-    email: email.value,
-    message: message.value,
-  };
-  const saveData = throttle(
-    localStorage.setItem('feedback-form-state', JSON.stringify(data)),
-    500
-  );
+  //const { input, message } = evt.currentTarget.elements;
+  const data = { email: input.value, message: message.value };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
 }
 
-if (localStorage.getItem('feedback-form-state') !== null) {
-  try {
-    const parsedData = JSON.parse(localStorage.getItem('feedback-form-state'));
-    //console.log(parsedData);
-    input.value = parsedData.email;
-    message.value = parsedData.message;
-  } catch (error) {
-    console.log(error.name);
-    console.log(error.message);
+function updateIntput() {
+  if (localStorage.getItem('feedback-form-state') !== null) {
+    try {
+      const parsedData = JSON.parse(
+        localStorage.getItem('feedback-form-state')
+      );
+      //console.log(parsedData);
+      input.value = parsedData.email;
+      message.value = parsedData.message;
+    } catch (error) {
+      console.log(error.name);
+      console.log(error.message);
+    }
+  } else {
+    input.value = '';
+    message.value = '';
   }
-} else {
-  input.value = '';
-  message.value = '';
 }
+updateIntput();
 
 function onSubmit(evt) {
   evt.preventDefault();
@@ -40,11 +41,11 @@ function onSubmit(evt) {
   if (email.value === '' || message.value === '') {
     return alert('Please fill in all the fields!');
   }
-  const data = {
+  const dataOutput = {
     email: email.value,
     message: message.value,
   };
-  console.log(data);
+  console.log(dataOutput);
   evt.currentTarget.reset();
   localStorage.removeItem('feedback-form-state');
 }
